c56af26bcdbb794d24e8cd0bdbf218bc84ee5794
nelsia <dev.nelsia@gmail.com>
2021-04-06 09:36:29 +0900

    init commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import router from './router'; // 追記
+
+Vue.config.productionTip = false
+
+new Vue({
+  router, // 追記
+  render: h => h(App),
+}).$mount('#app')
